5b7c0264199f7b4645548a739c98ec4335cb02a6
Dimo Markov <dimomarkov@gmail.com>
2024-02-24 01:25:17 +0200

    Improved test leak detection

--- test/TestMaterial.cpp
+++ test/TestMaterial.cpp
@@ -9,6 +9,7 @@
 #include <Entity/Thing.hpp>
 #include <catch2/catch.hpp>
 
+
 /// See https://github.com/catchorg/Catch2/blob/devel/docs/tostring.md        
 CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
    const Text serialized {ex};
@@ -48,21 +49,29 @@ constexpr auto MaterialCode = R"code(
 
 
 SCENARIO("Shader generation", "[materials]") {
-   Allocator::State memoryState;
+   static Allocator::State memoryState;
 
    for (int repeat = 0; repeat != 10; ++repeat) {
       GIVEN(std::string("Init and shutdown cycle #") + std::to_string(repeat)) {
          // Create root entity                                          
          Thing root;
          root.SetName("ROOT");
-
-         // Create runtime at the root                                  
          root.CreateRuntime();
-
-         // Load modules                                                
          root.LoadMod("FileSystem");
          root.LoadMod("AssetsImages");
          root.LoadMod("AssetsMaterials");
+         
+         WHEN("The material is created via abstractions") {
+            auto producedMaterial = root.CreateUnit<A::Material>(Code(MaterialCode));
+
+            // Update once                                              
+            root.Update(Time::zero());
+            root.DumpHierarchy();
+
+            REQUIRE(producedMaterial.GetCount() == 1);
+            REQUIRE(producedMaterial.CastsTo<A::Material>(1));
+            REQUIRE(producedMaterial.IsSparse());
+         }
 
       #if LANGULUS_FEATURE(MANAGED_REFLECTION)
          WHEN("The material is created via tokens") {
@@ -70,31 +79,13 @@ SCENARIO("Shader generation", "[materials]") {
             
             // Update once                                              
             root.Update(Time::zero());
-            
-            THEN("Various traits change") {
-               root.DumpHierarchy();
+            root.DumpHierarchy();
 
-               REQUIRE(producedMaterial.GetCount() == 1);
-               REQUIRE(producedMaterial.CastsTo<A::Material>(1));
-               REQUIRE(producedMaterial.IsSparse());
-            }
+            REQUIRE(producedMaterial.GetCount() == 1);
+            REQUIRE(producedMaterial.CastsTo<A::Material>(1));
+            REQUIRE(producedMaterial.IsSparse());
          }
       #endif
-
-         WHEN("The material is created via abstractions") {
-            auto producedMaterial = root.CreateUnit<A::Material>(Code(MaterialCode));
-
-            // Update once                                              
-            root.Update(Time::zero());
-
-            THEN("Various traits change") {
-               root.DumpHierarchy();
-
-               REQUIRE(producedMaterial.GetCount() == 1);
-               REQUIRE(producedMaterial.CastsTo<A::Material>(1));
-               REQUIRE(producedMaterial.IsSparse());
-            }
-         }
 
          // Check for memory leaks after each cycle                     
          REQUIRE(memoryState.Assert());
