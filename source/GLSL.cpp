ef3ed6a6c7d0dc4f37a4b362c363c9e3224784db
Epixu <dimomarkov@gmail.com>
2026-09-21 10:54:02 +0300

    Renames

--- source/GLSL.cpp
+++ source/GLSL.cpp
@@ -6,7 +6,7 @@
 /// SPDX-License-Identifier: GPL-3.0-or-later                                 
 ///                                                                           
 #include "GLSL.hpp"
-#include <Langulus/Annies/Edit.hpp>
+#include <Langulus/Edit.hpp>
 #include <Langulus/Verbs/Catenate.hpp>
 
 #define GLSL_VERBOSE(a) LANGULUS(NOOP)
